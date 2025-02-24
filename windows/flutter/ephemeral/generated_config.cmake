ab53b9d37dadd01feb9206caf1e77e2e31d9c8a9
saisrij0 <119802389+saisrij0@users.noreply.github.com>
2025-02-24 12:44:51 +0530

    Add files via upload

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "D:\\flutter_windows_3.22.0-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "D:\\pro\\internaltask" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=D:\\flutter_windows_3.22.0-stable\\flutter"
+  "PROJECT_DIR=D:\\pro\\internaltask"
+  "FLUTTER_ROOT=D:\\flutter_windows_3.22.0-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=D:\\pro\\internaltask\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=D:\\pro\\internaltask"
+  "FLUTTER_TARGET=D:\\pro\\internaltask\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9mNjM0NGI3NWRjZjg2MWQ4YmYxZjEzMjI3ODBiODgxMWY5ODJlMzFhLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=D:\\pro\\internaltask\\.dart_tool\\package_config.json"
+)
